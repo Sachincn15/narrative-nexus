002a240691043914f9bb4f7cd76d5852fa4c1365
Sachincn15 <sachincn1555@gmail.com>
2026-03-02 00:55:42 +0530

    Updated code

--- app.py
+++ app.py
@@ -270,17 +270,17 @@ elif selected == "Topic Modeling":
                 except ValueError:
                     st.error("Not enough clean text to generate topics.")
         with col2:
-    st.subheader("🧠 Summarization")
-    if st.button("Generate Summary"):
-        with st.spinner("AI is thinking..."):
-            try:
-                tokenizer, model = load_summarizer_model()
-                sample_df = df.sample(frac=1).reset_index(drop=True)
-                raw_text = " ".join(sample_df[text_col].astype(str).tolist())[:2000]
+            st.subheader("🧠 Summarization")
+            if st.button("Generate Summary"):
+            with st.spinner("AI is thinking..."):
+                try:
+                    tokenizer, model = load_summarizer_model()
+                    sample_df = df.sample(frac=1).reset_index(drop=True)
+                    raw_text = " ".join(sample_df[text_col].astype(str).tolist())[:2000]
                 
-                # Manual encoding and generation
-                inputs = tokenizer(raw_text, return_tensors="pt", max_length=1024, truncation=True)
-                summary_ids = model.generate(
+                    # Manual encoding and generation
+                    inputs = tokenizer(raw_text, return_tensors="pt", max_length=1024, truncation=True)
+                    summary_ids = model.generate(
                     inputs["input_ids"], 
                     max_length=150, 
                     min_length=40, 
@@ -292,7 +292,7 @@ elif selected == "Topic Modeling":
                 
                 st.markdown(f"""<div style="padding:20px; background-color:rgba(255, 49, 49, 0.1); border: 1px solid #ff3131; border-radius: 10px;"><h4 style="color:#ff3131;">AI Insight:</h4><p style="color:white;">{summary}</p></div>""", unsafe_allow_html=True)
             except Exception as e:
-                st.error(f"Error: {e}")
+            st.error(f"Error: {e}")
 
 # ==========================================
 # 4. SENTIMENT ANALYSIS (WITH EXPORT)
